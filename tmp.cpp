bde7122b408c1ead9872c36bd5c1028cd13eb657
Jacob Lambert <jacob@128-223-223-99-wireless.uoregon.edu>
2017-03-21 12:09:23 -0700

    adding tmp files

--- tmp.cpp
+++ tmp.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
-
-class A {
-  public:
-  int tmp(int x)
-  {
-    x = 56;
-
-    return x;
-  }
-
-};
-
 int main() {
 
-  A a;
-
-  std::cout << a.tmp(3);
+  #pragma omp parallel
+  {
+    std::cout << "Hi";
+  }
 }
